b474f4a3b21a81f0a90eb456323ffbd4040b5cdb
Blonicx <Belte_YT@outlook.com>
2024-12-25 02:08:41 +0100

    Init

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import { app, BrowserWindow } from 'electron';
+import path from 'node:path';
+import started from 'electron-squirrel-startup';
+
+if (started) {
+  app.quit();
+}
+
+const createWindow = () => {
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'backend/preload.js'),
+    },
+  });
+
+  mainWindow.loadFile(path.join(__dirname, 'src/index.html'));
+};
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
